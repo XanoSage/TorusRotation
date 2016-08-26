3d1787dbfdb4bb2ec3ada7658c13ba56c8e0d316
Alexander Khramtsov <khramtsov.olexander@gmail.com>
2016-08-26 13:06:42 +0300

    Was added Unity project, added ignore files, added necessary folders and scenes, added script, creating and initing point - in process;

---
+++ TorusRotation/Assets/Scripts/GeneralScripts/TorusBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TorusBehaviour : MonoBehaviour 
+{
+    [SerializeField] private int _segmentCounterSmall = 5;    
+    [SerializeField] private int _segmentCounterBig = 5;
+
+    [SerializeField] private float _smallRadius = 5f;
+    [SerializeField] private float _bigRadius = 15f;
+
+    [SerializeField] private GameObject _patternPoint;
+
+    [SerializeField] private Color[] _colors;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void CreateAndInitAllPoints()
+    {
+    }
+
+    private void Rotation()
+    {
+    }
+}
